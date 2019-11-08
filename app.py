eaea4b1b68a73e9895636cb30315732cdce12446
Afrochemist <yamaha366@aol.com>
2019-11-08 07:04:39 -0600

    will make changes later

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 #### Initial Page ####
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',title="Welcome Page")
 
 
 
@@ -15,14 +16,22 @@ def index():
 #### Sign Up ####
 @app.route('/signUp')
 def signUp():
-    return render_template('signUp.html')
+    return render_template('signUp.html', title="Sign up Page")
+
+
 
 #### Sign In ####
 
 @app.route('/signIn')
 def signIn():
-    return render_template('signIn.html')
+    return render_template('signIn.html', title="Sign In Page")
 
+
+
+#### Payment Page
+@app.route('/payment')
+def payment():
+    return render_template('payment.html', title='Payment Page')
 
 
 if __name__ == '__main__':
